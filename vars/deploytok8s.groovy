0e6a18d443e6a11d7ae9bc747ad2d8dcdc48f66b
DevOps Loc Ta <locduc@zenfolio.com>
2023-06-19 23:58:05 +0700

    update

--- vars/deploytok8s.groovy
+++ vars/deploytok8s.groovy
@@ -15,10 +15,17 @@ void call(Map demoBuild, String demoVersion) {
     String namespace = "demo"
     // Load list of container repositories. (DevOps Controlled)
 
-    withKubeConfig([credentialsId: 'k8sCredential']) {
+    kubeconfig(credentialsId: 'akstest', serverUrl: '') {
         sh "export registry=${demoRegistry}; export appname=${demoBuild.name}; export tag=${demoVersion}; \
         envsubst < .ci/deployment.yml > deployment.yml; envsubst < .ci/service.yml > service.yml"
         sh "kubectl apply -f deployment.yml -n ${namespace}"
         sh "kubectl apply -f service.yml -n ${namespace}"
     }
+
+    // withKubeConfig([credentialsId: 'k8sCredential']) {
+    //     sh "export registry=${demoRegistry}; export appname=${demoBuild.name}; export tag=${demoVersion}; \
+    //     envsubst < .ci/deployment.yml > deployment.yml; envsubst < .ci/service.yml > service.yml"
+    //     sh "kubectl apply -f deployment.yml -n ${namespace}"
+    //     sh "kubectl apply -f service.yml -n ${namespace}"
+    // }
 }
