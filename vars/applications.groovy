8ab9d704ea93b68d40ea56b03dc0f5901ea8a5f5
DevOps Loc Ta <locduc@zenfolio.com>
2023-06-19 16:28:23 +0700

    update

--- vars/applications.groovy
+++ vars/applications.groovy
@@ -4,54 +4,37 @@ void call(Map demoBuild, String demoVersion) {
     String orgRegistry = 'demo'
     String dockerHubCredential = 'dockerhub'
     String buildRegistry = 'gitlab.simplemdg.com:5050'
-    String gitlabCredential = 'gitlab_simplemdg_com'
-    String gitlabTokenCredential = 'gitlab_self_host_token'
-    String baseImage     = "${orgRegistry}/simplemdg_package"
-    String baseTag       = "16-full"
-    // Variables common
-    String apiCF = 'https://api.cf.us10-001.hana.ondemand.com'
     String flowName = "${demoBuild.build.flow.name}"
-    String artifact = "hello/mta_archives/hello_0.1.0.mtar"
-    String space = 'app'
-    String org = 'database'
+    String baseImage     = "mcr.microsoft.com/dotnet/sdk"
+    String baseTag       = "6.0"
     // Variables branch
     String checkBranches = "$env.BRANCH_NAME"
-    String[] deployBranches = ['main', 'develop']
+    String[] deployBranches = ['main', 'jenkins']
 
 //========================================================================
 //========================================================================
 
 //========================================================================
 //========================================================================
-    if (deployBranches.contains(env.BRANCH_NAME)) {
-        //====================================================================
-        stage ('Prepare Package') {
-            script {
-                writeFile file: 'deployment.sh', text: libraryResource('dev/demo/flows/simplemdg/script/deployment.sh')
+
+    stage ('Prepare Package') {
+        script {
+            writeFile file: '.ci/Dockerfile.SDK', text: libraryResource('dev/demo/flows/dotnet/docker/Dockerfile.SDK')
+            writeFile file: '.ci/Dockerfile.Runtime.API', text: libraryResource('dev/demo/flows/dotnet/docker/Dockerfile.Runtime.API')
+            writeFile file: '.ci/Dockerfile.package', text: libraryResource('dev/laidon/flows/backend/simplemdg_db_industry/docker/Dockerfile.package')
+        }
+    }
+
+    switch (flowName) {
+        case 'dotnet':
+            stage("Build Solution") {
+                docker.build("demo/${demoBuild.name}-sdk:${demoVersion}", "--force-rm --no-cache -f ./.ci/Dockerfile.SDK \
+                --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} ${WORKSPACE}") 
             }
-        }
-        //====================================================================
-        //====================================================================
-        // ====================================================================
-        stage('Build') {
-            sh 'mbt build -p cf -s hello'
-        }
-        //====================================================================
-        //====================================================================
-        // ====================================================================
-        //====================================================================
-        stage('Deploy') {
-            withCredentials([[$class        : 'UsernamePasswordMultiBinding',
-                credentialsId   : 'luketa',
-                usernameVariable: 'USERNAME',
-                passwordVariable: 'PASSWORD']]) {
-            //==================================================================== 
-                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: dockerHubCredential, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
-                        sh "chmod +x ./deployment.sh"
-                        command = "./deployment.sh -u ${USERNAME} -p ${PASSWORD} -o ${org} -s ${space} -c ${apiCF}"
-                        sh "docker run -v -i $baseImage:$baseTag $command"
-                    }
-                }
+            stage("Publish ${rt.name}") {
+                docker.build("demo/${demoBuild.name}:${demoVersion}", "--force-rm --no-cache -f ./.ci/Dockerfile.Runtime.API \
+                --build-arg BASEIMG=demo/${demoBuild.name}-sdk --build-arg IMG_VERSION=${demoVersion} \
+                --build-arg ENTRYPOINT=${demoBuild.build.runtime.name} --build-arg RUNIMG=${baseImage} --build-arg RUNVER=${baseTag} .")
             }
         }
     }
