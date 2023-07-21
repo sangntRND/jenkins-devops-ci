df0988b4de52e1e6c9f7dc5aeff464067a9dd965
DevOps Loc Ta <locduc@zenfolio.com>
2023-07-21 14:08:36 +0700

    update

--- vars/react.groovy
+++ vars/react.groovy
@@ -16,62 +16,64 @@ void call() {
 //========================================================================
 //========================================================================
 
-    stage ('Prepare Package') {
-        script {
-            writeFile file: '.ci/Dockerfile.ci', text: libraryResource('dev/demo/flows/react/docker/Dockerfile.ci')
-            writeFile file: '.ci/react_init.sh', text: libraryResource('dev/demo/flows/react/script/react_init.sh')
-            writeFile file: '.ci/nginx.conf', text: libraryResource('dev/demo/flows/react/script/nginx.conf')
-            writeFile file: '.ci/deployment.yml', text: libraryResource('deploy/fe/deployment.yml')
-            writeFile file: '.ci/service.yml', text: libraryResource('deploy/fe/service.yml')
-        }
-    }
-
-    stage ("Run Install") {
-        script {
-            sh "npm ci"
-        }
-    }
-
-    stage ('Run Build') {
-        script {
-            sh "npm run build"
-        }
-    }
-
-    stage ('Run Unit Tests') {
-        script {
-            sh "npm run test"
-        }
-    }
-
-    stage ('Run Integration Tests') {
-        echo "Run Integration Tests"
-    }
-
-    stage ("Build Docker Images") {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${demoRegistry}", acrCredential ) {
-                docker.build("${demoRegistry}/jenkins/${projectName}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.ci \
-                --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} .")
-            }
-        }
-    }
-
-    stage ("Push Docker Images") {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${demoRegistry}", acrCredential ) {
-                sh "docker push ${demoRegistry}/jenkins/${projectName}:${BUILD_NUMBER}"
-            }
-        }
-    }
-    // stage ("Deploy To K8S") {
-    //     kubeconfig(credentialsId: 'akstest', serverUrl: '') {
-    //         sh "export registry=${demoRegistry}; export appname=${projectName}; export tag=${BUILD_NUMBER}; \
-    //         envsubst < .ci/deployment.yml > deployment.yml; envsubst < .ci/service.yml > service.yml"
-    //         sh "kubectl apply -f deployment.yml -n ${namespace}"
-    //         sh "kubectl apply -f service.yml -n ${namespace}"
+    // stage ('Prepare Package') {
+    //     script {
+    //         writeFile file: '.ci/Dockerfile.ci', text: libraryResource('dev/demo/flows/react/docker/Dockerfile.ci')
+    //         writeFile file: '.ci/react_init.sh', text: libraryResource('dev/demo/flows/react/script/react_init.sh')
+    //         writeFile file: '.ci/nginx.conf', text: libraryResource('dev/demo/flows/react/script/nginx.conf')
+    //         writeFile file: '.ci/deployment.yml', text: libraryResource('deploy/fe/deployment.yml')
+    //         writeFile file: '.ci/service.yml', text: libraryResource('deploy/fe/service.yml')
     //     }
     // }
+
+    // stage ("Run Install") {
+    //     script {
+    //         sh "npm ci"
+    //     }
+    // }
+
+    // stage ('Run Build') {
+    //     script {
+    //         sh "npm run build"
+    //     }
+    // }
+
+    // stage ('Run Unit Tests') {
+    //     script {
+    //         sh "npm run test"
+    //     }
+    // }
+
+    // stage ('Run Integration Tests') {
+    //     echo "Run Integration Tests"
+    // }
+
+    // stage ("Build Docker Images") {
+    //     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+    //         docker.withRegistry("https://${demoRegistry}", acrCredential ) {
+    //             docker.build("${demoRegistry}/jenkins/${projectName}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.ci \
+    //             --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} .")
+    //         }
+    //     }
+    // }
+
+    // stage ("Push Docker Images") {
+    //     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+    //         docker.withRegistry("https://${demoRegistry}", acrCredential ) {
+    //             sh "docker push ${demoRegistry}/jenkins/${projectName}:${BUILD_NUMBER}"
+    //         }
+    //     }
+    // }
+    stage ("Deploy To K8S") {
+        withKubeConfig( clusterName: 'nttraining',
+                        contextName: 'nttraining',
+                        credentialsId: 'akstest', 
+                        namespace: 'demo', 
+                        restrictKubeConfigAccess: false,
+                        ) {
+            sh 'kubectl get node'
+        }
+    }
 }
 
 //========================================================================
