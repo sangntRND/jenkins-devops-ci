369ccccbd32f906fb2ccdbbbe0c2ed99665072ce
DevOps Loc Ta <locduc@zenfolio.com>
2023-07-21 15:16:32 +0700

    update

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 void call() {
     String runtime = "Microsoft.DSX.ProjectTemplate.API"
-    String publishProject = "src/BookStore.API/BookStore.API.csproj"
+    String publishProject = "ProjectTemplate/Microsoft.DSX.ProjectTemplate.API/Microsoft.DSX.ProjectTemplate.API.csproj"
     String baseImage     = "nttraining.azurecr.io/source/dotnet/sdk"
     String baseTag       = "6.0.411-jammy"
     String baseSonarTag  = "6.0.411-sonarqube"
