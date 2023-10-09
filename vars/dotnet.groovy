5c66e984f8512bda0a580df06153f07e67d57031
DevOps Loc Ta <locduc@zenfolio.com>
2023-10-09 22:29:59 +0700

    update

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -35,97 +35,98 @@ void call() {
             writeFile file: '.ci/deployment.yml', text: libraryResource('deploy/be/deployment.yml')
             writeFile file: '.ci/service.yml', text: libraryResource('deploy/be/service.yml')
             writeFile file: '.ci/html.tpl', text: libraryResource('dev/demo/flows/trivy/html.tpl')
-            // withCredentials([string(credentialsId: 'dbpasswd', variable: 'dbpasswd')]) {
-            //     sh "export Server=${server}; export User=${user}; export Database=${database}; export Password=${dbpasswd}; \
-            //     envsubst < ${csprojpath}/appsettings.jenkins.json > ${csprojpath}/appsettings.json; cat ${csprojpath}/appsettings.json"
-            // }
-        }
-    }
-
-    stage ("Trivy Scan Secret") {
-        script {
-            sh "trivy fs . --scanners secret --format template --template @.ci/html.tpl -o .ci/secretreport.html"
-            publishHTML (target : [allowMissing: true,
-                alwaysLinkToLastBuild: true,
-                keepAll: true,
-                reportDir: '.ci',
-                reportFiles: 'secretreport.html',
-                reportName: 'Trivy Secrets Report',
-                reportTitles: 'Trivy Secrets Report']
-            )
-        }
-    }
-
-    stage ("Build Solution") {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${demoRegistry}", acrCredential ) {
-                docker.build("${containerName}/${projectName}-sdk:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.SDK \
-                --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} ${WORKSPACE}")
+            withCredentials([string(credentialsId: 'connectionstrings', variable: 'connectionstrings')]) {
+                sh "export connectionstrings=${connectionstrings}; \
+                    envsubst < $(find . -type f -name appsettings.jenkins.json) > $(find . -type f -name appsettings.json); \
+                    cat $(find . -type f -name appsettings.json)"
             }
         }
     }
 
-    stage ('Run Unit Tests') {
-        sh "mkdir -p results"
-        sh "docker run -i --rm --volume './results:/src/results' ${containerName}/${projectName}-sdk:${BUILD_NUMBER} $rununitTest"
-    }
+    // stage ("Trivy Scan Secret") {
+    //     script {
+    //         sh "trivy fs . --scanners secret --format template --template @.ci/html.tpl -o .ci/secretreport.html"
+    //         publishHTML (target : [allowMissing: true,
+    //             alwaysLinkToLastBuild: true,
+    //             keepAll: true,
+    //             reportDir: '.ci',
+    //             reportFiles: 'secretreport.html',
+    //             reportName: 'Trivy Secrets Report',
+    //             reportTitles: 'Trivy Secrets Report']
+    //         )
+    //     }
+    // }
 
-    stage ('Run Integration Tests') {
-        echo "Run Integration Tests"
-        sh "ls -la ./results"
-    }
+    // stage ("Build Solution") {
+    //     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+    //         docker.withRegistry("https://${demoRegistry}", acrCredential ) {
+    //             docker.build("${containerName}/${projectName}-sdk:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.SDK \
+    //             --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} ${WORKSPACE}")
+    //         }
+    //     }
+    // }
 
-    stage ('Process Test Results') {
-        docker.image("${containerName}/${projectName}-sdk:${BUILD_NUMBER}").inside() {
-            xunit(
-                testTimeMargin: '600000',
-                thresholdMode: 1,
-                thresholds: [failed(), skipped()],
-                tools: [MSTest(deleteOutputFiles: true, failIfNotNew: true, pattern: "results/*.trx", skipNoTestFiles: false, stopProcessingIfError: true)]
-            )
-        }
+    // stage ('Run Unit Tests') {
+    //     sh "mkdir -p results"
+    //     sh "docker run -i --rm --volume './results:/src/results' ${containerName}/${projectName}-sdk:${BUILD_NUMBER} $rununitTest"
+    // }
 
-        cobertura coberturaReportFile: "results/*/*.xml"
-    }
+    // stage ('Run Integration Tests') {
+    //     echo "Run Integration Tests"
+    //     sh "ls -la ./results"
+    // }
 
-    stage('SonarQube analysis') {
-        script {
-            withCredentials([string(credentialsId: sonarToken, variable: 'SONAR_TOKEN')]) {
-                docker.build("${containerName}/${projectName}-sonar:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.SonarBuild \
-                --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseSonarTag} --build-arg SONAR_HOST=${sonarHost} --build-arg SONAR_PROJECT=${projectName} --build-arg SONAR_TOKEN=${SONAR_TOKEN} ${WORKSPACE}") 
-            }
-        }
-    }
+    // stage ('Process Test Results') {
+    //     docker.image("${containerName}/${projectName}-sdk:${BUILD_NUMBER}").inside() {
+    //         xunit(
+    //             testTimeMargin: '600000',
+    //             thresholdMode: 1,
+    //             thresholds: [failed(), skipped()],
+    //             tools: [MSTest(deleteOutputFiles: true, failIfNotNew: true, pattern: "results/*.trx", skipNoTestFiles: false, stopProcessingIfError: true)]
+    //         )
+    //     }
 
-    stage ("Build Docker Images Run Time") {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${demoRegistry}", acrCredential ) {
-                docker.build("${demoRegistry}/${containerName}/${projectName}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.Runtime.API \
-                --build-arg BASEIMG=${containerName}/${projectName}-sdk --build-arg IMG_VERSION=${BUILD_NUMBER} \
-                --build-arg ENTRYPOINT=${runtime} --build-arg PUBLISH_PROJ=${publishProject} --build-arg RUNIMG=${baseImage} --build-arg RUNVER=${baseTag} .")
-            }
-        }
-    }
+    //     cobertura coberturaReportFile: "results/*/*.xml"
+    // }
 
-    stage ("Trivy Scan Docker Images") {
-        sh "trivy image --scanners vuln,config --exit-code 0 --severity HIGH,CRITICAL --format template --template @.ci/html.tpl -o .ci/imagesreport.html ${demoRegistry}/${containerName}/${projectName}:${BUILD_NUMBER}"
-        publishHTML (target : [allowMissing: true,
-            alwaysLinkToLastBuild: true,
-            keepAll: true,
-            reportDir: '.ci',
-            reportFiles: 'imagesreport.html',
-            reportName: 'Trivy Vulnerabilities Images Report',
-            reportTitles: 'Trivy Vulnerabilities Images Report']
-        )
-    }
+    // stage('SonarQube analysis') {
+    //     script {
+    //         withCredentials([string(credentialsId: sonarToken, variable: 'SONAR_TOKEN')]) {
+    //             docker.build("${containerName}/${projectName}-sonar:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.SonarBuild \
+    //             --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseSonarTag} --build-arg SONAR_HOST=${sonarHost} --build-arg SONAR_PROJECT=${projectName} --build-arg SONAR_TOKEN=${SONAR_TOKEN} ${WORKSPACE}") 
+    //         }
+    //     }
+    // }
 
-    stage ("Push Docker Images") {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            docker.withRegistry("https://${demoRegistry}", acrCredential ) {
-                sh "docker push ${demoRegistry}/${containerName}/${projectName}:${BUILD_NUMBER}"
-            }
-        }
-    }
+    // stage ("Build Docker Images Run Time") {
+    //     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+    //         docker.withRegistry("https://${demoRegistry}", acrCredential ) {
+    //             docker.build("${demoRegistry}/${containerName}/${projectName}:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.Runtime.API \
+    //             --build-arg BASEIMG=${containerName}/${projectName}-sdk --build-arg IMG_VERSION=${BUILD_NUMBER} \
+    //             --build-arg ENTRYPOINT=${runtime} --build-arg PUBLISH_PROJ=${publishProject} --build-arg RUNIMG=${baseImage} --build-arg RUNVER=${baseTag} .")
+    //         }
+    //     }
+    // }
+
+    // stage ("Trivy Scan Docker Images") {
+    //     sh "trivy image --scanners vuln --exit-code 0 --severity HIGH,CRITICAL --format template --template @.ci/html.tpl -o .ci/imagesreport.html ${demoRegistry}/${containerName}/${projectName}:${BUILD_NUMBER}"
+    //     publishHTML (target : [allowMissing: true,
+    //         alwaysLinkToLastBuild: true,
+    //         keepAll: true,
+    //         reportDir: '.ci',
+    //         reportFiles: 'imagesreport.html',
+    //         reportName: 'Trivy Vulnerabilities Images Report',
+    //         reportTitles: 'Trivy Vulnerabilities Images Report']
+    //     )
+    // }
+
+    // stage ("Push Docker Images") {
+    //     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: acrCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+    //         docker.withRegistry("https://${demoRegistry}", acrCredential ) {
+    //             sh "docker push ${demoRegistry}/${containerName}/${projectName}:${BUILD_NUMBER}"
+    //         }
+    //     }
+    // }
     // stage ("Deploy To K8S") {
     //     withKubeConfig( caCertificate: '',
     //                     clusterName: "${k8scontextName}",
