011d113f8c31fbbaea07f73d26fc8c24c6d7a4dd
DevOps Loc Ta <locduc@zenfolio.com>
2023-10-09 23:32:31 +0700

    update

--- vars/react.groovy
+++ vars/react.groovy
@@ -148,7 +148,6 @@ void call() {
                             serverUrl: '') {
                 sh "export acrUrl=${demoRegistry}; export containerName=${containerName}; export projectname=${projectName}; export tag=${BUILD_NUMBER}; \
                 envsubst < .ci/deployment.yml > deployment.yml; envsubst < .ci/service.yml > service.yml"
-                sh "kubectl create ns demo"
                 sh "kubectl apply -f deployment.yml"
                 sh "kubectl apply -f service.yml"
             }
