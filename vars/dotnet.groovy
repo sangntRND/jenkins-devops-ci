acccd67b050c819a2a1d9033b78a80b697daa044
DevOps Loc Ta <locduc@zenfolio.com>
2023-10-09 22:50:43 +0700

    update

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -35,8 +35,7 @@ void call() {
             writeFile file: '.ci/deployment.yml', text: libraryResource('deploy/be/deployment.yml')
             writeFile file: '.ci/service.yml', text: libraryResource('deploy/be/service.yml')
             writeFile file: '.ci/html.tpl', text: libraryResource('dev/demo/flows/trivy/html.tpl')
-            appsettingsjenkins = sh returnStatus: true, script: """ find . -type f -name appsettings.jenkins.json """
-            echo "a ${appsettingsjenkins}"
+            sh returnStatus: true, script: """ find . -type f -name appsettings.jenkins.json """
             // withCredentials([string(credentialsId: 'connectionstrings', variable: 'connectionstrings')]) {
             //     sh "envsubst < ${(find . -type f -name appsettings.jenkins.json)} > ${(find . -type f -name appsettings.json)}; cat ${(find . -type f -name appsettings.json)}"
             // }
