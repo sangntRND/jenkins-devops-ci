bf4fac1dfa7608302039ea8686f7deb09cc8a8f3
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-13 23:51:57 +0700

    Init python pipeline

---
+++ vars/pythonPipeline.groovy
@@ -0,0 +1,59 @@
+#!/usr/bin/env groovy
+void call(Map pipelineParams) {
+
+//========================================================================
+
+//========================================================================
+
+    pipeline {
+
+        agent any
+
+        options {
+            disableConcurrentBuilds()
+            disableResume()
+            timeout(time: 1, unit: 'HOURS')
+        }
+        
+        stages {
+            stage ('Load Pipeline') {
+                when {
+                    allOf {
+                        // Condition Check
+                        anyOf{
+                            // Branch Event: Nornal Flow
+                            anyOf {
+                                branch 'main'
+                            }
+                            // Manual Run: Only if checked.
+                            allOf{
+                                triggeredBy 'UserIdCause'
+                            }
+                        }
+                    }
+                }
+                steps {
+                    script {
+                        echo "Hello world"
+                    }
+                }
+            }
+        }
+
+        post {
+            cleanup {
+                cleanWs()
+            }
+        }
+    }
+}
+//========================================================================
+// Demo CI
+// Version: v1.0
+// Updated:
+//========================================================================
+//========================================================================
+// Notes:
+//
+//
+//========================================================================
