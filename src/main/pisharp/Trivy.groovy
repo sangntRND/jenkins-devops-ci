36761b936090eee55da82193e9fa0a26ed1bdc8e
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 00:58:18 +0700

    Fix class import 7

--- src/main/pisharp/Trivy.groovy
+++ src/main/pisharp/Trivy.groovy
@@ -1,19 +1,17 @@
 package main.pisharp
 
-class Trivy {
-    def trivyScanSecret() {
-        stage ("Trivy Scan Secret") {
-            script {
-                sh "trivy fs . --scanners secret --format template --template @.ci/html.tpl -o .ci/secretreport.html"
-                publishHTML (target : [allowMissing: true,
-                    alwaysLinkToLastBuild: true,
-                    keepAll: true,
-                    reportDir: '.ci',
-                    reportFiles: 'secretreport.html',
-                    reportName: 'Trivy Secrets Report',
-                    reportTitles: 'Trivy Secrets Report']
-                )
-            }
+def trivyScanSecret() {
+    stage ("Trivy Scan Secret") {
+        script {
+            sh "trivy fs . --scanners secret --format template --template @.ci/html.tpl -o .ci/secretreport.html"
+            publishHTML (target : [allowMissing: true,
+                alwaysLinkToLastBuild: true,
+                keepAll: true,
+                reportDir: '.ci',
+                reportFiles: 'secretreport.html',
+                reportName: 'Trivy Secrets Report',
+                reportTitles: 'Trivy Secrets Report']
+            )
         }
     }
 }
