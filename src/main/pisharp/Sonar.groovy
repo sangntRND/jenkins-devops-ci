1af0e4936fba64a55dcce0ce0080cf9ca93f728c
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 22:47:46 +0700

    fix groovy syntax error 2

--- src/main/pisharp/Sonar.groovy
+++ src/main/pisharp/Sonar.groovy
@@ -4,25 +4,30 @@ def sonarQubeAnalysis(projectKey) {
     def SONAR_HOST_URL = 'http://13.213.249.3:9000/'
     stage('SonarQube analysis') {
         script {
-            withCredentials([string(credentialsId: "sonar-token", variable: 'SONAR_AUTH_TOKEN')]) {
-                // Run SonarQube Scanner inside a Docker container
-                sh """
-                docker run --rm -v ./:/usr/src -w /usr/src \
-                -e SONAR_HOST_URL=${SONAR_HOST_URL} \
-                -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
-                sonarsource/sonar-scanner-cli:latest \
-                sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.sources=. \
-                -Dsonar.exclusions=**/tests/** \
-                -Dsonar.host.url=${SONAR_HOST_URL} \
-                -Dsonar.login=${SONAR_AUTH_TOKEN} \
-                -Dsonar.python.coverage.reportPaths=results/coverage.xml
-                """
+            withSonarQubeEnv('My SonarQube Server'){
+                withCredentials([string(credentialsId: "sonar-token", variable: 'SONAR_AUTH_TOKEN')]) {
+                    // Run SonarQube Scanner inside a Docker container
+                    sh """
+                    docker run --rm -v ./:/usr/src -w /usr/src \
+                    -e SONAR_HOST_URL=${SONAR_HOST_URL} \
+                    -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
+                    sonarsource/sonar-scanner-cli:latest \
+                    sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.sources=. \
+                    -Dsonar.exclusions=**/tests/** \
+                    -Dsonar.host.url=${SONAR_HOST_URL} \
+                    -Dsonar.login=${SONAR_AUTH_TOKEN} \
+                    -Dsonar.python.coverage.reportPaths=results/coverage.xml
+                    """
+                }                
             }
+
         }
     }
     stage('Quality Gate') {
-        waitForQualityGate abortPipeline: true
+        timeout(time: 1, unit: 'HOURS') {
+            waitForQualityGate abortPipeline: true
+        }
     }
 }
