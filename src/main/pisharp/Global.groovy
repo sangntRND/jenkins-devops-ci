a20d597b9c5fcab90b41b132bfed93867b60a83c
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 22:17:26 +0700

    fix sed command error

--- src/main/pisharp/Global.groovy
+++ src/main/pisharp/Global.groovy
@@ -71,7 +71,7 @@ def deployToK8S(args){
 
                 // Update the image tag in the deployment YAML file
                 sh """
-                sed -i "s|\(image: [^:]*:\)[^ ]*|\1${newTag}|g"
+                    sed -i "s|\\(image: [^:]*:\\)[^ ]*|\\1${newTag}|g" ${deploymentYamlFile}
                 """
 
                 // Commit and push the changes
