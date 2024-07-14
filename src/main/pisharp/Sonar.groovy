3ba54dbeaaa3017037ab383bc733da53e1706580
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 23:05:39 +0700

    fix groovy syntax error 5

--- src/main/pisharp/Sonar.groovy
+++ src/main/pisharp/Sonar.groovy
@@ -7,11 +7,7 @@ def sonarQubeAnalysis(projectKey,sonarHostURL) {
             withSonarQubeEnv('SonarQube'){
                 // Run SonarQube Scanner inside a Docker container
                 sh """
-                docker run --rm -v ./:/usr/src -w /usr/src \
-                -e SONAR_HOST_URL=${env.SONAR_HOST_URL} \
-                -e SONAR_LOGIN=${env.SONAR_AUTH_TOKEN} \
-                sonarsource/sonar-scanner-cli:latest \
-                sonar-scanner \
+                ${env.SONARQUBE_SCANNER_HOME}/bin/sonar-scanner \
                 -Dsonar.projectKey=${projectKey} \
                 -Dsonar.sources=. \
                 -Dsonar.exclusions=**/tests/** \
