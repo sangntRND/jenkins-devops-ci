503f3d2d10acd5c84c39f676579cb7232e3d194b
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 22:57:19 +0700

    fix groovy syntax error 4

--- src/main/pisharp/Sonar.groovy
+++ src/main/pisharp/Sonar.groovy
@@ -5,22 +5,20 @@ def sonarQubeAnalysis(projectKey,sonarHostURL) {
     stage('SonarQube analysis') {
         script {
             withSonarQubeEnv('SonarQube'){
-                withCredentials([string(credentialsId: "sonar-token", variable: 'SONAR_AUTH_TOKEN')]) {
-                    // Run SonarQube Scanner inside a Docker container
-                    sh """
-                    docker run --rm -v ./:/usr/src -w /usr/src \
-                    -e SONAR_HOST_URL=${SONAR_HOST_URL} \
-                    -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
-                    sonarsource/sonar-scanner-cli:latest \
-                    sonar-scanner \
-                    -Dsonar.projectKey=${projectKey} \
-                    -Dsonar.sources=. \
-                    -Dsonar.exclusions=**/tests/** \
-                    -Dsonar.host.url=${SONAR_HOST_URL} \
-                    -Dsonar.login=${SONAR_AUTH_TOKEN} \
-                    -Dsonar.python.coverage.reportPaths=results/coverage.xml
-                    """
-                }                
+                // Run SonarQube Scanner inside a Docker container
+                sh """
+                docker run --rm -v ./:/usr/src -w /usr/src \
+                -e SONAR_HOST_URL=${env.SONAR_HOST_URL} \
+                -e SONAR_LOGIN=${env.SONAR_AUTH_TOKEN} \
+                sonarsource/sonar-scanner-cli:latest \
+                sonar-scanner \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.sources=. \
+                -Dsonar.exclusions=**/tests/** \
+                -Dsonar.host.url=${env.SONAR_HOST_URL} \
+                -Dsonar.login=${env.SONAR_AUTH_TOKEN} \
+                -Dsonar.python.coverage.reportPaths=results/coverage.xml
+                """         
             }
 
         }
