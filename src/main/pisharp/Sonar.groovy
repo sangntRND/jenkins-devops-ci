5b97213195373afd933f1ed5110109d75ede77f6
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 15:06:53 +0700

    Fix the syntax error

--- src/main/pisharp/Sonar.groovy
+++ src/main/pisharp/Sonar.groovy
@@ -1,19 +1,19 @@
 package main.pisharp
 
 def sonarQubeAnalysis() {
-    stage('SonarQube Analysis') {
-        // Run SonarQube Scanner inside a Docker container
-        sh """
-        docker run --rm -v $(pwd):/usr/src -w /usr/src \
-        -e SONAR_HOST_URL=${SONAR_HOST_URL} \
-        -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
-        sonarsource/sonar-scanner-cli:latest \
-        sonar-scanner \
-        -Dsonar.projectKey=projecttemplate-python1-api \
-        -Dsonar.sources=. \
-        -Dsonar.host.url=${SONAR_HOST_URL} \
-        -Dsonar.login=${SONAR_AUTH_TOKEN} \
-        -Dsonar.python.coverage.reportPaths=results/coverage.xml
-        """
-    }
+        script {
+            // Run SonarQube Scanner inside a Docker container
+            sh """
+            docker run --rm -v $(pwd):/usr/src -w /usr/src \
+            -e SONAR_HOST_URL=${SONAR_HOST_URL} \
+            -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
+            sonarsource/sonar-scanner-cli:latest \
+            sonar-scanner \
+            -Dsonar.projectKey=projecttemplate-python1-api \
+            -Dsonar.sources=. \
+            -Dsonar.host.url=${SONAR_HOST_URL} \
+            -Dsonar.login=${SONAR_AUTH_TOKEN} \
+            -Dsonar.python.coverage.reportPaths=results/coverage.xml
+            """
+        }
 }
