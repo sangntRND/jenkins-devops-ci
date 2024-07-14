7f105510815f0a056cd009327bd68ebb60178527
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 15:37:33 +0700

    Fix the syntax error 7

--- src/main/pisharp/Sonar.groovy
+++ src/main/pisharp/Sonar.groovy
@@ -2,22 +2,23 @@ package main.pisharp
 
 def sonarQubeAnalysis() {
     def SONAR_HOST_URL = 'http://13.213.249.3:9000/'
-    def SONAR_AUTH_TOKEN = 'heelo'
     stage('SonarQube analysis') {
         script {
-            // Run SonarQube Scanner inside a Docker container
-            sh """
-            docker run --rm -v ./:/usr/src -w /usr/src \
-            -e SONAR_HOST_URL=${SONAR_HOST_URL} \
-            -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
-            sonarsource/sonar-scanner-cli:latest \
-            sonar-scanner \
-            -Dsonar.projectKey=projecttemplate-python1-api \
-            -Dsonar.sources=. \
-            -Dsonar.host.url=${SONAR_HOST_URL} \
-            -Dsonar.login=${SONAR_AUTH_TOKEN} \
-            -Dsonar.python.coverage.reportPaths=results/coverage.xml
-            """
+            withCredentials([string(credentialsId: sonarToken, variable: 'SONAR_AUTH_TOKEN')]) {
+                // Run SonarQube Scanner inside a Docker container
+                sh """
+                docker run --rm -v ./:/usr/src -w /usr/src \
+                -e SONAR_HOST_URL=${SONAR_HOST_URL} \
+                -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
+                sonarsource/sonar-scanner-cli:latest \
+                sonar-scanner \
+                -Dsonar.projectKey=projecttemplate-python1-api \
+                -Dsonar.sources=. \
+                -Dsonar.host.url=${SONAR_HOST_URL} \
+                -Dsonar.login=${SONAR_AUTH_TOKEN} \
+                -Dsonar.python.coverage.reportPaths=results/coverage.xml
+                """
+            }
         }
     }
 }
