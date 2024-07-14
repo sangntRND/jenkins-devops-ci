3b722a1f607a8f19b5f15dd91e1237ab566e722a
Sang Nguyen Thanh <sang211nt@gmail.com>
2024-07-14 15:13:13 +0700

    Fix the syntax error 2

--- src/main/pisharp/Sonar.groovy
+++ src/main/pisharp/Sonar.groovy
@@ -4,18 +4,7 @@ def sonarQubeAnalysis() {
     stage('SonarQube analysis') {
         script {
             // Run SonarQube Scanner inside a Docker container
-            sh """
-            docker run --rm -v $(pwd):/usr/src -w /usr/src \
-            -e SONAR_HOST_URL=${SONAR_HOST_URL} \
-            -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} \
-            sonarsource/sonar-scanner-cli:latest \
-            sonar-scanner \
-            -Dsonar.projectKey=projecttemplate-python1-api \
-            -Dsonar.sources=. \
-            -Dsonar.host.url=${SONAR_HOST_URL} \
-            -Dsonar.login=${SONAR_AUTH_TOKEN} \
-            -Dsonar.python.coverage.reportPaths=results/coverage.xml
-            """
+            sh "docker run --rm -v $(pwd):/usr/src -w /usr/src -e SONAR_HOST_URL=${SONAR_HOST_URL} -e SONAR_LOGIN=${SONAR_AUTH_TOKEN} sonarsource/sonar-scanner-cli:latest sonar-scanner -Dsonar.projectKey=my-flask-app -Dsonar.sources=. -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN} -Dsonar.python.coverage.reportPaths=results/sonar-coverage.xml"
         }
     }
 }
